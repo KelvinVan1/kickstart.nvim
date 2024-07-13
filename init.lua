6a1f15b3f9d04c13ba7085258086b6e8a22a02d4
Kelvin Van <kelvinvan.cs@gmail.com>
2024-07-13 14:31:31 -0700

    Transparency and colorschemes added

--- init.lua
+++ init.lua
@@ -773,22 +773,35 @@ require('lazy').setup({
     end,
   },
 
+  -- Catppucin ColorScheme (Default Starting)
   { -- You can easily change to a different colorscheme.
     -- Change the name of the colorscheme plugin below, and then
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
+    'catppuccin/nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'catppuccin'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
     end,
+  },
+
+  -- Tokyonight ColorScheme
+  {
+    'folke/tokyonight.nvim',
+    priority = 1000,
+  },
+
+  -- Rose-Pine ColorScheme
+  {
+    'rose-pine/neovim',
+    priority = 1000,
   },
 
   -- Highlight todo, notes, etc in comments
@@ -879,6 +892,11 @@ require('lazy').setup({
   -- require 'kickstart.plugins.autopairs',
   -- require 'kickstart.plugins.neo-tree',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+
+  -- Transparency toggle for background
+  {
+    'xiyaowong/transparent.nvim',
+  },
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
